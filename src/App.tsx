1430cca578fb17d618196d70e78b870e888305ca
fankaljead <30434925+fankaljead@users.noreply.github.com>
2024-12-16 12:32:54 +0800

    fix: 解决vercel依赖报错

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 import { Board } from './components/Board'
 import { Controls } from './components/Controls'
